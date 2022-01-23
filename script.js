1af731d9d04ec9a9b7eb7a5e6bff89f66c5d71a8
Victor <94798862+victorhe33@users.noreply.github.com>
2022-01-22 19:09:29 -0500

    MOD added reset button+customizable grid
    added reset button that prompts customizable grid.

--- script.js
+++ script.js
@@ -9,25 +9,54 @@ body.appendChild(title);
 //create container Div and append to body
 const containerDiv = document.createElement("div");
 containerDiv.setAttribute('class', 'grid-container');
+containerDiv.style.cssText = 'max-width: 100px; height:100px;';
 body.appendChild(containerDiv);
 
-//make, label, append 16 grid divs to container div
-for (i = 1; i < 17; i++) {
-  //make div
-  const div = document.createElement("div");
-  
-  //set grid div attributes
-  div.setAttribute('id', `${i}`);
-  div.setAttribute('class', `grid-box`)
-  // div.textContent = i;
-  
-  //append div to container.
-  containerDiv.appendChild(div);
+//container resize function;
+function resizeContainer (gridwidth) {
+  let width = gridwidth * 25;
+  let height = width;
+
+  containerDiv.style.cssText = `max-width: ${width}px; height:${height}px;`
 }
 
+//make, label, append x grid divs to container div
+function customizeGrid(answer) {
+  let gridWidth = answer;
+
+  resizeContainer(gridWidth);
+  makeGrid(gridWidth)
+}
+
+function makeGrid (num) {
+  let gridNum = 16;
+
+  if (num !== undefined) {
+    gridNum = num ** 2;
+  }
+
+  for (i = 0; i < gridNum; i++) {
+    //make div
+    const div = document.createElement("div");
+    
+    //set grid div attributes
+    div.setAttribute('id', `${i}`);
+    div.setAttribute('class', `grid-box`)
+    
+    //append div to container.
+    containerDiv.appendChild(div);
+  }
+
+  const targetDiv = document.querySelectorAll('.grid-box');
+  targetDiv.forEach(div => div.addEventListener('mouseenter', handleHover));
+}
+
+//make Grid function called on load so that listeners are after.
+makeGrid();
+
 //hover effect
-const targetDiv = document.querySelectorAll('.grid-box');
-targetDiv.forEach(div => div.addEventListener('mouseenter', handleHover));
+// const targetDiv = document.querySelectorAll('.grid-box');
+// targetDiv.forEach(div => div.addEventListener('mouseenter', handleHover));
 
 function handleHover (e) {
   const hoveredDiv = e.target;
@@ -42,3 +71,29 @@ function handleHover (e) {
 }
 
 //reset and customizer button;
+const resetButton = document.createElement('button');
+resetButton.addEventListener('click', handleClick);
+resetButton.textContent = "Reset";
+resetButton.classList.add('reset-button');
+body.appendChild(resetButton);
+
+function handleClick () {
+  // targetDiv.forEach(div => div.style['background-color'] = 'white');
+  let answer = prompt("Choose size of grid", "1 - 30");
+  
+  //sanitize answer;
+  answer = Number(answer);
+  if (answer >= 1 
+    && answer <= 30 
+    && typeof answer === "number") {
+    removeDivs();
+    customizeGrid(answer);
+  } else {
+    alert("Invalid selection")
+  }
+}
+
+function removeDivs () {
+  const deleteDivs = document.querySelectorAll('.grid-box')
+  deleteDivs.forEach(div => containerDiv.removeChild(div));
+}
