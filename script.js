cbace386c5c80c3e2cb47ece3b47c3d45112e39f
Victor <94798862+victorhe33@users.noreply.github.com>
2022-01-23 02:10:20 -0500

    MOD major overhaul
    -fixed grid to be static
    -added grid overlay feature
    -added slider control for grid size.
    -added buttons/toggle panel (color picker, rainbow, eraser, size viewer)

--- script.js
+++ script.js
@@ -1,101 +1,110 @@
-//node selectors for body;
-const body = document.querySelector("body");
+//VARIABLES
+let boxAcross = 5;
+let gridNum = boxAcross ** 2;
+let containerWidth = 600;
+let height = containerWidth / boxAcross;
+let width = height;
+let color = "black"
+let rainbow = false;
 
-//create title
-const title = document.createElement("h1");
-title.textContent = "Etch-A-Sketch";
-body.appendChild(title);
+//DOM SELECTORS
+const containerDiv = document.getElementById("gridContainer");
+const colorPicker = document.getElementById("colorPicker");
+const rainbowButton = document.querySelector("#rainbowButton");
+const eraserButton = document.getElementById("eraserButton");
+const resetButton = document.getElementById("resetButton");
+const slider = document.getElementById("slider");
+const sliderDisplay = document.getElementById("sliderDisplay");
+const toggleCheckbox = document.getElementById("toggleCheckbox");
 
-//create container Div and append to body
-const containerDiv = document.createElement("div");
-containerDiv.setAttribute('class', 'grid-container');
-containerDiv.style.cssText = 'max-width: 100px; height:100px;';
-body.appendChild(containerDiv);
+//DISPLAYS
+sliderDisplay.textContent = `${slider.value} x ${slider.value}`;
+slider.oninput = function () {
+  sliderDisplay.textContent = `${slider.value} x ${slider.value}`;
+  //update specs
+  boxAcross = this.value;
+  gridNum = boxAcross ** 2;
+  height = containerWidth / boxAcross;
+  width = height;
 
-//container resize function;
-function resizeContainer (gridwidth) {
-  let width = gridwidth * 25;
-  let height = width;
-
-  containerDiv.style.cssText = `max-width: ${width}px; height:${height}px;`
+  removeDivs();
+  makeGrid();
 }
 
-//make, label, append x grid divs to container div
-function customizeGrid(answer) {
-  let gridWidth = answer;
-
-  resizeContainer(gridWidth);
-  makeGrid(gridWidth)
-}
-
-function makeGrid (num) {
-  let gridNum = 16;
-
-  if (num !== undefined) {
-    gridNum = num ** 2;
-  }
+//FUNCTIONS
+function makeGrid() {
+  //make grid
 
   for (i = 0; i < gridNum; i++) {
     //make div
     const div = document.createElement("div");
-    
+
     //set grid div attributes
     div.setAttribute('id', `${i}`);
     div.setAttribute('class', `grid-box`)
-    
+    div.setAttribute('style', `min-height: ${height}px; min-width: ${width}px;`)
+    // div.textContent = i; //delete later
+
     //append div to container.
     containerDiv.appendChild(div);
+
+    //keep grid overlay state steady
+    if (toggleCheckbox.checked === true) {
+       div.classList.add("toggle-grid")
+    }
   }
 
   const targetDiv = document.querySelectorAll('.grid-box');
   targetDiv.forEach(div => div.addEventListener('mouseenter', handleHover));
 }
 
-//make Grid function called on load so that listeners are after.
-makeGrid();
-
-//hover effect
-// const targetDiv = document.querySelectorAll('.grid-box');
-// targetDiv.forEach(div => div.addEventListener('mouseenter', handleHover));
-
-function handleHover (e) {
-  const hoveredDiv = e.target;
-  let color = "";
-  console.log(hoveredDiv.style['background-color']);
-
-  if (hoveredDiv.style['background-color'] === "") {
-      color = "black";
-    } else {
-    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-      color = `#${randomColor}`;
-    }
-  hoveredDiv.style['background-color'] = color;
-}
-
-//reset and customizer button;
-const resetButton = document.createElement('button');
-resetButton.addEventListener('click', handleClick);
-resetButton.textContent = "Reset";
-resetButton.classList.add('reset-button');
-body.appendChild(resetButton);
-
-function handleClick () {
-  // targetDiv.forEach(div => div.style['background-color'] = 'white');
-  let answer = prompt("Choose size of grid", "1 - 30");
-  
-  //sanitize answer;
-  answer = Number(answer);
-  if (answer >= 1 
-    && answer <= 30 
-    && typeof answer === "number") {
-    removeDivs();
-    customizeGrid(answer);
-  } else {
-    alert("Invalid selection")
-  }
-}
-
-function removeDivs () {
+function removeDivs() {
   const deleteDivs = document.querySelectorAll('.grid-box')
   deleteDivs.forEach(div => containerDiv.removeChild(div));
 }
+
+function handleHover(e) {
+  const hoveredDiv = e.target;
+   
+  if (rainbow) {
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    color = `#${randomColor}`;
+  }
+
+  hoveredDiv.style['background-color'] = color;
+}
+
+function handleColorPicker () {
+  color = colorPicker.value;
+}
+
+function handleRainbowClick () {
+    if (rainbow) {
+      rainbow = false;
+      color = "black";
+    }
+    else {
+      rainbow = true;
+    }
+}
+
+function handleResetClick () {
+  removeDivs();
+  toggleCheckbox.checked = false;
+  makeGrid();
+}
+
+function handleToggleClick () {
+  const divGrid = document.querySelectorAll('.grid-box')
+  divGrid.forEach(div => div.classList.toggle("toggle-grid"));
+}
+
+//EVENT LISTENERS
+colorPicker.addEventListener('change', handleColorPicker);
+rainbowButton.addEventListener('click', handleRainbowClick);
+eraserButton.addEventListener('click', () => color = "white");
+resetButton.addEventListener('click', handleResetClick);
+toggleCheckbox.addEventListener('change', handleToggleClick)
+
+//initiate grid
+makeGrid();
